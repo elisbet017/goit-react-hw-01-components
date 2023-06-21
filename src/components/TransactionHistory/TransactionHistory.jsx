881e85890dd507e854f242f108ad3a704a7bbb99
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-06-21 22:59:27 +0200

    Add all files for fourth task

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+import css from './TransactionHistory.module.css'
+// import PropTypes from 'prop-types'
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.thead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
