968ddc3eb511ea5312b25e352a6b272d78b925c6
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-06-22 22:14:35 +0200

    Fix propTypes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -24,7 +24,7 @@ export const Statistics = ({ title, stats }) => {
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.arrayOf(PropTypes.shape({
+  stats: PropTypes.arrayOf(PropTypes.exact({
     id: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     percentage: PropTypes.number.isRequired,
